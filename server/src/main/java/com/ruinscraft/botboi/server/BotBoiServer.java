9afb9a1812e87c8ece7bc778ee4cb8fac02a0e8f
Dexton Anderson <contact@dextonanderson.com>
2018-06-05 14:00:04 -0500

    start twitter impl

--- server/src/main/java/com/ruinscraft/botboi/server/BotBoiServer.java
+++ server/src/main/java/com/ruinscraft/botboi/server/BotBoiServer.java
@@ -11,9 +11,17 @@ public class BotBoiServer implements Runnable {
 	private String discordToken;
 	private String key;
 	
+	private static BotBoiServer instance;
+	
+	public static BotBoiServer getInstance() {
+		return instance;
+	}
+	
 	public BotBoiServer(String discordToken, String key) {
 		this.discordToken = discordToken;
 		this.key = key;
+		
+		instance = this;
 	}
 	
 	public String getDiscordToken() {
@@ -23,7 +31,7 @@ public class BotBoiServer implements Runnable {
 	public String getKey() {
 		return key;
 	}
-
+	
 	@Override
 	public void run() {
 		JDA jda = null;
